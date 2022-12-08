9ce721cd7c6745e0397784d9676805aa768398f4
filipljung <fille-1998@hotmail.com>
2022-12-08 13:47:57 +0000

    added welcome print

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Wellcome to quiz game !!")
+print('NOTE: if your spelling is incorrect then it is considered as wrong answer')
+score = 0
+question_no = 0
+playing = input('Do you want to play ? ').lower()
+if playing == 'yes':
+    question_no += 1
+    ques = input(f'\n{question_no}. what does CPU stand for? ').lower()
+    if ques == 'central processing unit':
+        score +=1
+        print('correct! you got 1 point')
