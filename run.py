6af9d7056122f71dc2a308303f6128c7d4153e50
filipljung <fille-1998@hotmail.com>
2022-12-09 14:15:53 +0000

    solving error problems

--- run.py
+++ run.py
@@ -24,39 +24,39 @@ def ask_question(question, options):
 questions = [
   {
   print("Welcome to the quiz! Please read the questions carefully and enter the number of your answer.")
-  "question": "Where is Fashion Capital of the world?",
-  "options": ["Paris", "London", "Rome"],
-  "answer": 0
+    "question": "Where is Fashion Capital of the world?",
+    "options": ["Paris", "London", "Rome"],
+    "answer": 0
   },
   {
   print("Please read the questions carefully and enter the number of your answer.")
-  "question": "What is the largest ocean on earth?",
-  "options": ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean"],
-  "answer": 0
+    "question": "What is the largest ocean on earth?",
+    "options": ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean"],
+    "answer": 0
   },
   {
   print("Please read the questions carefully and enter the number of your answer.")
-  "question": "What is the name of the longest river in the world?",
-  "options": ["Mississippi", "Amazon", "Nile"],
-  "answer": 2
+    "question": "What is the name of the longest river in the world?",
+    "options": ["Mississippi", "Amazon", "Nile"],
+    "answer": 2
   },
   {
   print("Please read the questions carefully and enter the number of your answer.")
-  "question": "What is the capital of uk?",
-  "options": ["Paris", "London", "Rome"],
-  "answer": 1
+    "question": "What is the capital of uk?",
+    "options": ["Paris", "London", "Rome"],
+    "answer": 1
   },
   {
   print("Please read the questions carefully and enter the number of your answer.")
-  "question": "What is the highest mountain in the world?",
-  "options": ["Lhotse", "K2", "Mount everest"],
-  "answer": 2
+    "question": "What is the highest mountain in the world?",
+    "options": ["Lhotse", "K2", "Mount everest"],
+    "answer": 2
   },
   {
   print("Please read the questions carefully and enter the number of your answer.")
-  "question": "What is the largest company of the world?",
-  "options": ["Amazon", "Walmart", "Apple"],
-  "answer": 2
+    "question": "What is the largest company of the world?",
+    "options": ["Amazon", "Walmart", "Apple"],
+    "answer": 2
   }
 ]
   
